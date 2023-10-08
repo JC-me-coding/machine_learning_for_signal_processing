bee0f0d5b292a37464e434c517865e786590e05d
simoneiriksson <simoneiriksson@gmail.com>
2023-10-08 16:42:52 +0200

    Fixing small stuff

--- 2p5_wiener_filter.py
+++ 2p5_wiener_filter.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import utils
+from numpy.lib.stride_tricks import sliding_window_view
 
 root = r'data/'
 
@@ -20,13 +21,24 @@ w_n = noise_mat['noise']
 # proudly stolen from ex3.2.8.py
 x_n = s_n + w_n
 
-plt.plot(x_n) 
+
+plt.plot(x_n)
 plt.show()
-r_xx = utils.xcorr(x_n, x_n, 2)
-r_sx = utils.xcorr(s_n, x_n, 2)
 
-R_xx = np.hstack((r_xx[2:5], r_xx[1:4], r_xx[0:3]))
+k = 5
+r_xx = utils.xcorr(x_n, x_n, k)
+r_sx = utils.xcorr(s_n, x_n, k)
+R_xx = sliding_window_view(r_xx[:, 0], k+1)
+w_hat = np.linalg.solve(R_xx, r_sx[k:])
+R_xx.shape
 
-# the line below does not run
-#w_hat = np.linalg.solve(R_xx, r_dx[2:5])
+padded_x_n = np.pad(x_n[:, 0], k, mode='constant', constant_values=(0,0))
+X = sliding_window_view(padded_x_n, k+1)
+
+X.shape
+w_hat.shape
+s_hat = X@w_hat
+
+err = np.mean((s_hat - s_n)**2)
+
 
