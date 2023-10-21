c628921e679c551994a6c92d3f6bc1cdcfcf5d48
Julia Cathrine Youngman <juca@nnit.com>
2023-10-21 11:46:56 +0200

    uupdate 2p7

--- 2p7_adaptive_filtering.py
+++ 2p7_adaptive_filtering.py
@@ -3,6 +3,7 @@ import numpy as np
 import utils
 from numpy.lib.stride_tricks import sliding_window_view
 import soundfile as sf # only needed if playing does not work
+np.random.seed(42)
 
 root = r'data/'
 
@@ -13,22 +14,36 @@ signal_mat.keys()
 s_n = signal_mat['speech']
 sf.write(root + 'problem2_7_speech.wav', s_n, 8000, subtype='PCM_24')
 
-
 # load the filters
 lpir_file = root + 'problem2_7_lpir.mat'
 hpir_file = root + 'problem2_7_hpir.mat'
 bpir_file = root + 'problem2_7_bpir.mat'
 
 mat = utils.loadmatlabfile(lpir_file)
-print(mat.keys())
 lpir = mat['lpir']
 
 mat = utils.loadmatlabfile(hpir_file)
-print(mat.keys())
 hpir = mat['hpir']
 
 mat = utils.loadmatlabfile(bpir_file)
-print(mat.keys())
 bpir = mat['bpir']
+
+# make some noise
+mu, sigma = np.mean(s_n), np.var(s_n)
+x_noise = np.random.normal(mu, sigma, s_n.shape)
+sf.write(root + 'problem2_7_noise.wav', x_noise, 8000, subtype='PCM_24')
+
+
+# filter the noise signal through one of the filters
+k = lpir.shape[0]
+padded_x_noise = np.pad(x_noise[:, 0], (k,0), mode='constant', constant_values=(0,0))
+X = sliding_window_view(padded_x_noise, k)
+#print(f"{X.shape = }")
+#print(f"{lpir.shape = }")
+X @ lpir
+
+#add filtered noise to speech signal
+
+
 
 
